b2e288ffcbabd058b3fe05c0e6579ad0777fb000
banxi1988 <banxi1988@gmail.com>
2018-04-22 11:13:30 +0800

    add p344 and go solution

---
+++ answers/go/p344-reverse-string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func reverseString(s string) string {
+	bytes := []byte(s)
+	for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
+		tmp := bytes[i]
+		bytes[i] = bytes[j]
+		bytes[j] = tmp
+	}
+	return string(bytes)
+}
+
+func main() {
+	fmt.Println("olleh -> ", reverseString("hello"))
+}
