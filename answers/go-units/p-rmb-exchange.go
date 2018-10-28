87d83c79353a252b9ec458643df0ef7725a5ed8f
banxi1988 <banxi1988@gmail.com>
2018-10-28 16:19:45 +0800

    add 简单找零问题的贪心解法

--- answers/go-units/p-rmb-exchange.go
+++ answers/go-units/p-rmb-exchange.go
@@ -1,21 +1,24 @@
 package leetcode
 
-const rmbBaseCount = 6
+var rmbBases = [...]int{1, 2, 5, 10, 20, 50, 100}
 
-var rmbBases = [rmbBaseCount]int{1, 5, 10, 20, 50, 100}
+const rmbBaseCount = len(rmbBases)
 
-func fewestRmbExhcange(amount int) int {
-	count := 0
+// FewestRmbExhcange 返回最小的找零方案
+func FewestRmbExhcange(amount int) []int {
+	exchanges := []int{}
 	remain := amount
+
 	for remain > 0 {
 		for i := rmbBaseCount - 1; i > -1; {
-			if remain >= rmbBases[i] {
-				remain = remain - rmbBases[i]
-				count++
+			base := rmbBases[i]
+			if remain >= base {
+				remain = remain - base
+				exchanges = append(exchanges, base)
 			} else {
 				i--
 			}
 		}
 	}
-	return count
+	return exchanges
 }
--- answers/go-units/p-rmb-exchange_test.go
+++ answers/go-units/p-rmb-exchange_test.go
@@ -1,42 +1,39 @@
 package leetcode
 
 import "testing"
+import "github.com/stretchr/testify/assert"
 
-func Test_fewestRmbExhcange(t *testing.T) {
+func Test_FewestRmbExhcange(t *testing.T) {
 	type args struct {
 		amount int
 	}
 	tests := []struct {
-		name string
 		args args
-		want int
+		want []int
 	}{
-		{"1", args{1}, 1},
-		{"2", args{2}, 2},
-		{"3", args{3}, 3},
-		{"4", args{4}, 4},
-		{"5", args{5}, 1},
-		{"6", args{6}, 2},
-		{"7", args{7}, 3},
-		{"8", args{8}, 4},
-		{"9", args{9}, 5},
-		{"10", args{10}, 1},
-		{"11", args{11}, 2},
-		{"12", args{12}, 3},
-		{"13", args{13}, 4},
-		{"14", args{14}, 5},
-		{"15", args{15}, 2},
-		{"16", args{16}, 3},
-		{"17", args{17}, 4},
-		{"100", args{100}, 1},
-		{"103", args{103}, 4},
-		{"200", args{200}, 2},
+		{args{1}, []int{1}},
+		{args{2}, []int{2}},
+		{args{3}, []int{2, 1}},
+		{args{4}, []int{2, 2}},
+		{args{5}, []int{5}},
+		{args{6}, []int{5, 1}},
+		{args{7}, []int{5, 2}},
+		{args{8}, []int{5, 2, 1}},
+		{args{9}, []int{5, 2, 2}},
+		{args{10}, []int{10}},
+		{args{11}, []int{10, 1}},
+		{args{12}, []int{10, 2}},
+		{args{13}, []int{10, 2, 1}},
+		{args{14}, []int{10, 2, 2}},
+		{args{15}, []int{10, 5}},
+		{args{16}, []int{10, 5, 1}},
+		{args{17}, []int{10, 5, 2}},
+		{args{100}, []int{100}},
+		{args{103}, []int{100, 2, 1}},
+		{args{200}, []int{100, 100}},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := fewestRmbExhcange(tt.args.amount); got != tt.want {
-				t.Errorf("fewestRmbExhcange() = %v, want %v", got, tt.want)
-			}
-		})
+		got := FewestRmbExhcange(tt.args.amount)
+		assert.ElementsMatch(t, tt.want, got)
 	}
 }
