fe878c7c66628ebc69920f8ef56b03ad0aa34b6d
banxi1988 <banxi1988@gmail.com>
2018-12-05 20:45:01 +0800

    add p692 来自 SQL group by + order by  + limit 风格的简洁解法

--- answers/python3/test_p692_top_k_frequent_words.py
+++ answers/python3/test_p692_top_k_frequent_words.py
@@ -3,6 +3,8 @@
 __author__ = 'banxi'
 
 """
+LeetCode 692. 前K个高频单词 @by 代码会说话
+
 给一非空的单词列表，返回前 k 个出现次数最多的单词。
 
 返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
@@ -30,54 +32,28 @@ __author__ = 'banxi'
 """
 
 
-class TrieNode:
-    def __init__(self,ch:str):
-        self.ch = ch
-        self.count = 0
-        self.children = {} # type Dict[str,TrieNode]
-
-    def insert(self, word:str):
-        """插入一个单词"""
-        parent = self
-        for ch in word:
-            node = parent.children.get(ch)
-            if not node:
-                node = TrieNode(ch)
-                parent.children[ch] = node
-            parent = node
-        parent.count+=1
-
-
 from typing import List
-from collections import defaultdict
+from collections import defaultdict, Counter
+
 
 class Solution:
     def topKFrequent(self, words:List[str], k:int) -> List[str]:
-        word_to_count = defaultdict(int)
-        for word in words:
-            word_to_count[word] += 1
-        counts = sorted(set(word_to_count.values()),reverse=True)
-        count_to_words = defaultdict(list)
-        for word,count in word_to_count.items():
-            count_to_words[count].append(word)
-        total_count = 0
-        freq_index = 0
-        freq_words = []
-        while total_count < k:
-            count = counts[freq_index]
-            tmp_words = count_to_words[count]
-            words_count = len(tmp_words)
-            if words_count > 1:
-                tmp_words.sort()
-            if total_count + words_count> k:
-                nums = k -total_count
-                freq_words.extend(tmp_words[:nums])
-                total_count += nums
-            else:
-                freq_words.extend(tmp_words)
-                total_count += words_count
-            freq_index+=1
-        return  freq_words
+        """
+        select word,count(word) as cnt from words
+        group by word
+        order by cnt desc,word asc
+        limit 3
+        """
+        # i: 2
+        # love: 2
+        word_to_count = Counter(words)
+        freq_words = sorted(word_to_count.keys(),key=lambda word:(-word_to_count[word],word))
+        return freq_words[:k]
+
+
+
+
+
 
 
 
@@ -86,6 +62,6 @@ class Solution:
 def test_top_k_frequent():
     s = Solution()
     assert s.topKFrequent(["i", "love", "leetcode", "i", "love", "coding"],3) == ["i", "love","coding"]
-    # assert s.topKFrequent(["i"],1) == ["i"]
-    # assert s.topKFrequent(["i", "love", "leetcode", "i", "love", "coding"],2) == ["i", "love"]
-    # assert s.topKFrequent(["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],4) == ["the", "is", "sunny", "day"]
+    assert s.topKFrequent(["i"],1) == ["i"]
+    assert s.topKFrequent(["i", "love", "leetcode", "i", "love", "coding"],2) == ["i", "love"]
+    assert s.topKFrequent(["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],4) == ["the", "is", "sunny", "day"]
