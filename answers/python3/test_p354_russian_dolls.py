f6724f02fe8f7e7ec59b5140e8979be7aea4ff74
banxi1988 <banxi1988@gmail.com>
2018-12-13 20:23:39 +0800

    perf p354 llis 的动态规划解法

--- answers/python3/test_p354_russian_dolls.py
+++ answers/python3/test_p354_russian_dolls.py
@@ -17,49 +17,50 @@ LeetCode 354. 俄罗斯套娃信封问题 题解 by 代码会说话
 输入: envelopes = [[5,4],[6,4],[6,7],[2,3]]
 输出: 3 
 解释: 最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+(2,3)
+(5,4)
+(6,4)
+(6,7)
 """
 
 from typing import List
-from collections import defaultdict
 
-class Solution:
-  def maxEnvelopes(self, envelopes:List[List[int]]) -> int:
-    w_to_h = defaultdict(list)
-    h_to_w = defaultdict(list)
-    for w,h in envelopes:
-      w_to_h[w].append(h)
-      h_to_w[h].append(w)
-    # for h_list in w_to_h.values():
-    #   h_list.sort()
-    # for w_list in h_to_w.values():
-    #   w_list.sort()
-    widths = sorted(w_to_h.keys())
-    heights = sorted(h_to_w.keys())
-    wi = 0
-    pairs = set()
-    prev_hi = 0
-    w_count = len(widths)
-    h_count = len(heights)
-    while wi < w_count:
-      hi = prev_hi
-      w = widths[wi]
-      while hi < h_count:
-        h = heights[hi]
-        ws = h_to_w[h]
-        if w in ws:
-          pairs.add((w,h))
-          prev_hi = hi + 1
-          break
-        hi += 1
-      wi += 1
 
-    return len(pairs)
+class Solution(object):
+  def lengthOfLIS(self, envs:List[List[int]]) -> int:
+    n = len(envs)
+    brothers = [0] * n
+    brothers[0] = 1
+    max_count = 0
+    for i in range(1,n):
+      bro_count = 0
+      iw,ih = envs[i]
+      for j in range(0,i):
+        jw,jh = envs[j]
+        if jw < iw and jh < ih:
+          bro_count = max(bro_count,brothers[j])
+      bro_count += 1
+      brothers[i] = bro_count
+      max_count = max(max_count, bro_count)
+    return max_count
+
+  def maxEnvelopes(self, envs:List[List[int]]):
+    n = len(envs)
+    if n < 2:
+      return n
+    envs.sort(key=lambda t:(t[0],t[1]))
+    return self.lengthOfLIS(envs)
 
 
 
 def test():
   s = Solution()
+  assert s.maxEnvelopes( [[1,1]]) == 1
+  envelopes2 = [[1,15],[7,18],[7,6],[7,100],[2,200],[17,30],[17,45],[3,5],[7,8],[3,6],[3,10],[7,20],[17,3],[17,45]]
+  assert s.maxEnvelopes(envelopes2) == 3
   envelopes1 = [[2,100],[3,200],[4,300],[5,500],[5,400],[5,250],[6,370],[6,360],[7,380]]
   assert s.maxEnvelopes(envelopes1) == 5
+
   assert s.maxEnvelopes( [[5,4],[6,4],[6,7],[2,3]]) == 3
+  assert s.maxEnvelopes( [[5,4],[6,4],[6,5],[6,7],[2,3]]) == 3
   assert s.maxEnvelopes( [[5,4],[6,4]]) == 1
