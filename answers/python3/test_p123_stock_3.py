68d96f71724661747b5df61cae600ca7e0248c0d
banxi1988 <banxi1988@gmail.com>
2018-12-17 13:34:33 +0800

    perf p123 修正超时的问题

--- answers/python3/test_p123_stock_3.py
+++ answers/python3/test_p123_stock_3.py
@@ -56,7 +56,8 @@ class Solution:
     key_points = []
     for price in prices[1:]:
       if price < peek:
-        key_points.extend([valley,peek])
+        if peek > valley:
+          key_points.extend([valley,peek])
         valley=price
         peek=0
       else:
@@ -88,6 +89,8 @@ class Solution:
 
 def test():
   s = Solution()
+  assert s.maxProfit(make_large_prices()) == 0
+  assert s.maxProfit([5,4,3,2,1,0,0]) == 0
   assert s.maxProfit([1,2,4,2,5,7,2,4,9,0]) == 13
   assert s.maxProfit([7,1,5,6,5,5,6,2,7]) == 10
   assert s.maxProfit([7,1,5,6,5,4,6,2,7]) == 10
@@ -97,3 +100,8 @@ def test():
   assert s.maxProfit([3,3,5,0,0,3,1,4]) == 6
 
 # 199 / 200 个通过测试用例
+
+def make_large_prices():
+  l1 = list(range(10000,0,-1))
+  l2 = [0] * 16003
+  return l1 + l2
