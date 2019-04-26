9a232e5a12e7975bbfcaa6f25c8e6497fee0a8dd
banxi1988 <banxi1988@gmail.com>
2019-04-27 06:32:40 +0800

    add p1023 fsm solution and notes

--- answers/python3/array-and-str/test_p1023_camel_match.py
+++ answers/python3/array-and-str/test_p1023_camel_match.py
@@ -3,19 +3,56 @@
 __author__ = '代码会说话'
 
 """
+[LeetCode 1023]. 驼峰式匹配-正则及有限状态机解法[6分]
+
 如果我们可以将小写字母插入模式串 pattern 得到待查询项 query，那么待查询项与给定模式串匹配。
 （我们可以在任何位置插入每个字符，也可以插入 0 个字符。）
 
 给定待查询列表 queries，和模式串 pattern，返回由布尔值组成的答案列表 answer。
 只有在待查项 queries[i] 与模式串 pattern 匹配时， answer[i] 才为 true，否则为 false。
 
- ["CompetitiveProgramming","CounterPick","ControlPanel"]
+示例 1：
+
+输入：queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FB"
+输出：[true,false,true,true,false]
+示例：
+"FooBar" 可以这样生成："F" + "oo" + "B" + "ar"。
+"FootBall" 可以这样生成："F" + "oot" + "B" + "all".
+"FrameBuffer" 可以这样生成："F" + "rame" + "B" + "uffer".
+示例 2：
+
+输入：queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBa"
+输出：[true,false,true,false,false]
+解释：
+"FooBar" 可以这样生成："Fo" + "o" + "Ba" + "r".
+"FootBall" 可以这样生成："Fo" + "ot" + "Ba" + "ll".
+示例 3：
+
+输出：queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBaT"
+输入：[false,true,false,false,false]
+解释： 
+"FooBarTest" 可以这样生成："Fo" + "o" + "Ba" + "r" + "T" + "est".
+ 
+
+提示：
+
+1 <= queries.length <= 100
+1 <= queries[i].length <= 100
+1 <= pattern.length <= 100
+所有字符串都仅由大写和小写英文字母组成。
+
+
+["CompetitiveProgramming","CounterPick","ControlPanel"]
   "CooP"
-C
-  [false,false,true]
+  
+ 
+          C  ->   o  -> o   -> P ->
+ [a-z]* + C + o -> o ->   P -> 
+ Finite State Machine
 """
 
 from typing import List
+import re
 
 class Fsm:
   def __init__(self,pattern:str):
@@ -25,52 +62,38 @@ class Fsm:
 
   def reset(self):
     self.statei = -1
-    self.nextstate = None
 
-  def _can_auto_move_to_next(self):
-    return self.statei < (self.N -1) and (not self.pattern[self.statei+1].isupper())
+  def next_state(self):
+    nexti = self.statei + 1
+    if nexti < self.N:
+      return self.pattern[nexti]
 
-  def _next_state(self):
-    if self.statei < (self.N - 1):
-      return self.pattern[self.statei + 1]
-
-
-  def move_to(self,ch:str):
-    ns = self._next_state()
+  def can_move_to_next_state(self,ch:str):
+    ns = self.next_state()
     if ch.isupper():
-      if not ns:
-        return False
-      if ns != ch:
+      if ch != ns:
         return False
       self.statei += 1
     else:
-      # 后面可以匹配什么问题的小写字符
       if ns == ch:
-        if self._can_auto_move_to_next():
-          self.statei += 1
+        self.statei += 1
     return True
 
-  def match(self,query):
+  def match(self,query:str):
     self.reset()
     for ch in query:
-      if not self.move_to(ch):
+      if not self.can_move_to_next_state(ch):
         return False
-    return self.statei == self.N -1
-
-
-
-
-
-
-
+    return self.statei + 1 == self.N
 
 
 class Solution:
   def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
+    fsm = Fsm(pattern)
     ans = [False] * len(queries)
-    fsm = Fsm(pattern=pattern)
     for i,query in enumerate(queries):
       ans[i] = fsm.match(query)
+
     return ans
 
 
@@ -79,13 +102,12 @@ def test():
   s = Solution()
   true = True
   false = False
-  assert s.camelMatch(["CompetitiveProgramming","CounterPick","ControlPanel"],
-                      "CooP") == [false,false,true]
-  assert s.camelMatch(["aksvbjLiknuTzqon","ksvjLimflkpnTzqn","mmkasvjLiknTxzqn","ksvjLiurknTzzqbn","ksvsjLctikgnTzqn","knzsvzjLiknTszqn"],
-  "ksvjLiknTzqn") == [true,true,true,true,true,true]
+  assert s.camelMatch(["ControlPanel","CompetitiveProgramming","CounterPick"],
+                      "CooP") == [true,false,false]
   assert s.camelMatch(queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FB") == [true,false,true,true,false]
 
   assert s.camelMatch(queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBa") == [true,false,true,false,false]
 
   assert s.camelMatch(queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBaT") == [false,true,false,false,false]
 
+  assert s.camelMatch(["aksvbjLiknuTzqon","ksvjLimflkpnTzqn","mmkasvjLiknTxzqn","ksvjLiurknTzzqbn","ksvsjLctikgnTzqn","knzsvzjLiknTszqn"], "ksvjLiknTzqn") == [true,true,true,true,true,true]
